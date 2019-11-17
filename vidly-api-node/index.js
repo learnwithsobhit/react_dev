453f2030c869a7603a9c2ac2e791ecd1b6e87911
shobhit <learnwithshobhit@gmail.com>
2019-11-17 11:34:23 +0530

    dev:first update

--- vidly-api-node/index.js
+++ vidly-api-node/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const config = require("config");
 const app = express();
 
+//added to git
+
 require("./startup/logging")();
 require("./startup/cors")(app);
 require("./startup/routes")(app);
@@ -12,7 +14,7 @@ require("./startup/validation")();
 
 const port = process.env.PORT || config.get("port");
 const server = app.listen(port, () =>
-  winston.info(`Listening on port ${port}...`)
+    winston.info(`Listening on port ${port}...`)
 );
 
 module.exports = server;
